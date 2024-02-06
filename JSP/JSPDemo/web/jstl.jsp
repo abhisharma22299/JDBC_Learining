10e023814f1f38e1caf654f9b7bfeb7e461343c5
AbhiSharma22299 <abhisheksharmabca2020@gmail.com>
2024-02-06 20:41:23 +0530

    JDBC

--- JSP/JSPDemo/web/jstl.jsp
+++ JSP/JSPDemo/web/jstl.jsp
@@ -5,7 +5,7 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-
+ <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
     <head>
@@ -17,6 +17,5 @@
              
         <c:set var="name" value="Abhishek_Sharma" ></c:set>
         <c:out value="${name}"></c:out>
-        
     </body>
 </html>
--- JSP/jstl/src/main/java/Tags/TagHandler.java
+++ JSP/jstl/src/main/java/Tags/TagHandler.java
@@ -1,5 +1,24 @@
 package Tags;
 
-public class TagHandler {
+import jakarta.servlet.jsp.JspException;
+import jakarta.servlet.jsp.JspWriter;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+public class TagHandler extends TagSupport {
+
+	@Override
+	public int doStartTag() throws JspException {
+	
+	try {
+		// task .. tags 
+			JspWriter out=pageContext.getOut();
+			out.print("<h1>THis is custom tag</h1>");
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+		
+		return SKIP_BODY;
+	}
 
 }
